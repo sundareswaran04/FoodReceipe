85531b5ab1c07c14ca62e06691331f80cecf6470
unknown <sundarm9345@gmail.com>
2024-12-30 14:16:21 -0800

    Port changes

--- backend/server.js
+++ backend/server.js
@@ -11,19 +11,20 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3200;
 
-
-app.use(cors()); 
+// Middleware
+app.use(cors()); // Allow specific origins
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-const uri = "mongodb+srv://new-user-01:test123@foodrecipe.nr26n.mongodb.net/?retryWrites=true&w=majority&appName=FoodRecipe";
+// MongoDB Connection
+const uri ="mongodb+srv://new-user-01:test123@foodrecipe.nr26n.mongodb.net/?retryWrites=true&w=majority&appName=FoodRecipe";
 
 mongoose.set('strictQuery', false);
 mongoose.connect(uri, {
-  serverSelectionTimeoutMS: 10000,
+  serverSelectionTimeoutMS: 10000, 
 })
-  .then(() => console.log('MongoDB connected...'))
-  .catch(err => console.error('Error connecting to MongoDB:', err));
+.then(() => console.log('MongoDB connected...'))
+.catch(err => console.error('Error connecting to MongoDB:', err));
 
 
 app.use(express.static(path.join(__dirname, '../frontend')));
@@ -49,21 +50,21 @@ const userSchema = new mongoose.Schema({
   password: {
     type: String,
     required: true,
-    minlength: 6
+    minlength: 6 
   }
 });
 
 const commentSchema = new mongoose.Schema({
-  email: {
+  email:{
     type: String,
     required: true,
     match: /^[^\s@]+\@[^\s@]+\.[^\s@]+$/ // Email format validation
   },
-  name: {
+  name:{
     type: String,
     required: true
   },
-  comment: {
+  comment:{
     type: String,
     required: true
   },
@@ -73,7 +74,7 @@ const commentSchema = new mongoose.Schema({
     min: 1,
     max: 5
   },
-  recipeId: {
+  recipeId:{
     type: String,
     required: true
   }
@@ -165,19 +166,19 @@ app.post('/comment-post', async (req, res) => {
 });
 
 app.get('/comment-section', async (req, res) => {
-  const Id = req.query.Id;
+  const Id  = req.query.Id;
 
   if (!Id) {
-
+  
     return res.status(400).json({ error: `Recipe ID is required${Id}` });
   }
 
   try {
     const comments = await Comment.find({ recipeId: Id });
     if (comments.length === 0) {
-      return res.json([]);
+      return res.json([]); 
     }
-    res.json(comments);
+    res.json(comments); 
   } catch (error) {
     console.error('Error fetching comments:', error);
     res.status(500).json({ error: 'Error fetching comments' });
@@ -185,3 +186,6 @@ app.get('/comment-section', async (req, res) => {
 });
 
 
+app.listen(port, () => {  
+  console.log(`Server running on port ${port}`);
+})
